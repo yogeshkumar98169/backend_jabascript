b861984625554ec2af65e0a709387112910e4ceb
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2024-03-20 11:38:01 +0530

    Connective data Successfully

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log("Hello world")
+// require('dotenv').config({path:'./env'})
+
+// import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+// dotenv.config({
+//     path:'./env'
+// })
+
+connectDB();
+
+
+
+
+
+
+
+
+/*
+import express from 'express'
+const app=express();
+
+function connnectD(){
+
+}
+//!semicolon because problem aa jati hai
+;(async()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("Error : ",error)
+        throw error
+       })
+
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on http://localhost:${process.env.PORT}`)
+       })
+    }
+    catch(error){
+        console.log("ERROR : ",error)
+        throw err
+    }
+
+})()
+*/
