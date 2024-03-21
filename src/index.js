593402bdbecdafd599542513bc1f154519e30f75
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2024-03-21 12:11:17 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // require('dotenv').config({path:'./env'})
 
-// import dotenv from 'dotenv'
+import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-// dotenv.config({
-//     path:'./env'
-// })
+dotenv.config({
+    path:'./env'
+})
 
 connectDB()
     .then(() => {
