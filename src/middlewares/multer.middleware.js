80b11c10fca3e8aaff51e4351559a172529d5744
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2024-03-27 14:09:08 +0530

    multer done

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null,"./public/temp")    // we keep the file inside temp folder inside public
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)  //see more options
+    }
+})
+
+export const upload = multer({ storage })
