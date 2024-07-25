a5125b9f6bc3ed8e1645221a98ae1ee5f0764f29
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2024-07-25 17:16:42 +0530

    learn more about cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,6 +7,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+//localfilepath : server pr jo upload kiya hai uska path
 const uploadOnCloudinary=async(localFilePath)=>{
     try{
         if(!localFilePath){
