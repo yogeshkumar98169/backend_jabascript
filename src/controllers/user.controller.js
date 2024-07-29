8f11a33e7b7fbf76562c5dbc4cd4ec756842b4c6
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2024-07-29 15:33:18 +0530

    test app : take email, name postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,29 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
 
 //!We have 4 parameters err,req,res, next
+const registerUser = asyncHandler(async (req, res) => {
+   const{fullName, email,username, password}=req.body;
+   console.log(email)
+   console.log(fullName)
+   if(fullName===""){
+    throw new ApiError(400,"full name is required")
+   }
+})
+
+export { registerUser }
+
+
+
+
+
+
+
+
+
+/**
+ * We tested this app first
+ * !We have 4 parameters err,req,res, next
 const registerUser = asyncHandler(async (req, res) => {
     //!here we also send status and then send json data like we don res.json()
     res.status(200).json(
@@ -9,5 +32,4 @@ const registerUser = asyncHandler(async (req, res) => {
         }
     )
 })
-
-export { registerUser }
+ */
