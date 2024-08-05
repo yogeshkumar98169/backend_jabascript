4de0aa18023ceb3f7c57a560c797be95eddf75e6
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2024-08-05 15:46:22 +0530

    create route for login and log out

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verityJWT } from "../middlewares/auth.middleware.js";
 const router=Router()
 
 router.route("/register").post(
@@ -17,6 +18,10 @@ router.route("/register").post(
     registerUser)
 //!Here url is http://localhost:8000/api/v1/users/register pr register user hoga
 
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verityJWT,logoutUser)
 
 
 export default router;
